4439ed3aeedffb41ec6183fba145901211ecb554
haesol822 <dordork@naver.com>
2023-08-13 21:39:55 +0900

    first commit

---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import styles from "@src/styles/Favorites.module.css"
+import NewFile from '@src/components/NewFile';
+import { BiSearch } from "react-icons/bi";
+
+interface SearchBarProps {
+  onSearch: (searchTerm: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+    onSearch(value);
+  };
+
+  return (
+    <div className={styles.inputContainer}>
+      <BiSearch className={styles.iconSearch}></BiSearch>
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요..."
+        value={searchTerm}
+        onChange={handleInputChange}
+        className={styles.searchBar}
+      />
+    </div>
+  );
+};
+
+
+function Favorites() {
+  const handleSearch = (searchTerm: string) => {
+    console.log('검색어:', searchTerm);
+    // 검색 로직 구현
+  };
+
+  return (
+    <>
+      <div className={styles.root}>
+        <div className={styles.header}>
+          <div className={styles.title}>즐겨 찾기</div>
+          <SearchBar onSearch={handleSearch} />
+        </div>
+        <div className={styles.body}>
+          <div className={styles.titleWrapper}>
+            <div className={styles.titles}>
+              <div className={styles.fileName}>파일 이름</div>
+              <div className={styles.fileDate}>생성 일자</div>
+              <div className={styles.fileLength}>길이</div>
+            </div>
+            <hr className={styles.breakline}></hr>
+          </div>
+          <div className={styles.filesWrapper}>
+            <NewFile />
+            <NewFile />
+            <NewFile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Favorites;
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+function Landing() {
+  return (
+    <div>
+      랜딩 페이지입니다
+    </div>
+  );
+}
+
+export default Landing;

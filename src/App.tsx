ec3e9efe34d93b4a2dfbeea10e4bf166da58a1bf
haesol822 <dordork@naver.com>
2023-08-17 16:54:24 +0900

    로그인 회원가입 폼, 근데 오류 많음

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // import { useState } from 'react'
-import './App.css'
-import SideNav from './components/Wokspace/SideNav'
-import Home from './pages/Workspace/Home'
-import New from './pages/Workspace/New'
-import Detail from './pages/Workspace/Detail'
-import Favorites from './pages/Workspace/Favorites'
-import All from './pages/Workspace/All'
-import Loading from './pages/Workspace/Loading'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css';
+import SideNav from './components/Wokspace/SideNav';
+import Home from './pages/Workspace/Home';
+import New from './pages/Workspace/New';
+import Detail from './pages/Workspace/Detail';
+import Favorites from './pages/Workspace/Favorites';
+import All from './pages/Workspace/All';
+import Loading from './pages/Workspace/Loading';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 // import Landing from './pages/Landing'
 
 function App() {
@@ -22,6 +24,8 @@ function App() {
           <Route path="/home/detail" element={<Detail />} />
           <Route path="/home/favorites" element={<Favorites />} />
           <Route path="/home/loading" element={<Loading />} />
+          <Route path="/home/login" element={<Login />} />
+          <Route path="/home/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Login/Login.tsx
+++
@@ -1,5 +0,0 @@
-const Login = () => {
-    return <>로그인페이지 입니다.</>;
-};
-
-export default Login;

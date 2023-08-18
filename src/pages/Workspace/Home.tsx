329de0e036d46fa17fdc70cfd209787fe5ea3d58
최서현 <csh200310@naver.com>
2023-08-18 17:05:22 +0900

    fix:newfiles

--- src/pages/Workspace/Home.tsx
+++ src/pages/Workspace/Home.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import styles from "@src/styles/Home.module.css"
-import NewFile from '@src/components/Wokspace/NewFile';
+//import NewFile from '@src/components/Wokspace/NewFile';
+import { FaMicrophone } from "react-icons/fa";
 import { BiSearch } from "react-icons/bi";
 import axios from 'axios';
 import { useAtom } from 'jotai';
@@ -33,14 +34,19 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   );
 };
 
+const handleSearch = (searchTerm: string) => {
+  console.log('검색어:', searchTerm);
+  // 검색 로직 구현
+};
+
 
 function Home() {
   const [userId] = useAtom(userIdAtom);
 
   const fetchDataWithUserId = async () => {
     try {
-      const res = await axios.get(`https://www.assum.store/data?userId=${userId}/all`);
-      console.log(res.data);
+      const res = await axios.get(`https://www.assum.store/${userId}/home`);
+      console.log('홈으로 이동', res);
     } catch (err) {
       console.error('서버 요청 실패:', err);
     }
@@ -52,10 +58,31 @@ function Home() {
     }
   }, [userId]);
 
-  const handleSearch = (searchTerm: string) => {
-    console.log('검색어:', searchTerm);
-    // 검색 로직 구현
-  };
+  function NewFile() {
+    return (
+      <div className={styles.newroot}>
+        <div className={styles.newbody}>
+          <div className={styles.newfilesWrapper}>
+            <div className={styles.newicon}>
+              <FaMicrophone className={styles.newiconVoice}></FaMicrophone>
+            </div>
+            <div className={styles.newfileNameWrapper}>
+              <p className={styles.newfileName}>새로운 파일 1</p>
+              <p className={styles.newfileSub}>6호 태풍 '카눈'이 북상하며 영남 내륙을 통과중이다. 태풍은 대구 북서쪽을 지나갔고, 북서쪽으로 이동하 ...</p>
+            </div>
+          </div>
+          <div className={styles.newfileDate}>
+            <p>2023.08.10 12:39</p>
+          </div>
+          <div className={styles.newfileLength}>
+            <p>12분</p>
+          </div>
+        </div>
+        <hr className={styles.newbreakline}></hr>
+      </div>
+    );
+  }
+
 
   return (
     <>

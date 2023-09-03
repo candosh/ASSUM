68de35889571211b40b6f969e47ad27e5c8fdb6b
haesol822 <dordork@naver.com>
2023-09-03 15:25:27 +0900

    #38 feat: 메인페이지 워드클라우드 구현

--- src/pages/Workspace/Home.tsx
+++ src/pages/Workspace/Home.tsx
@@ -2,10 +2,16 @@ import { useState, useEffect } from 'react';
 import styles from "@src/styles/Home.module.css"
 import axios from 'axios';
 import SideNav from '@src/components/Wokspace/SideNav';
+import WordCloud from 'react-wordcloud';
 
 type KeywordRank = {
   keyword: string;
   count: number;
+}
+
+type Word = {
+  text: string;
+  value: number;
 }
 
 type ApiResponse = {
@@ -69,8 +75,16 @@ export default function Home() {
     { label: '30대', age: 30 },
     { label: '40대', age: 40 },
     { label: '50대', age: 50 },
-    { label: '60대 이상', age: 60 },
+    { label: '60대 ~', age: 60 },
   ];
+
+  // KeywordRank 배열을 Word 배열로 변환하는 함수
+  const convertToWords = (keywordRanks: KeywordRank[]): Word[] => {
+    return keywordRanks.map((rank) => ({
+      text: rank.keyword,
+      value: rank.count,
+    }));
+  };
 
   return (
     <>
@@ -79,54 +93,66 @@ export default function Home() {
         <div className={styles.header}>
           <div className={styles.title}>요약 키워드 순위</div>
         </div>
-        <div className={styles.wordCloudBox}>
-
-        </div>
-        <div className={styles.keywordBox}>
-          <div>
-            <p><h3>나이대별 키워드 순위</h3></p>
-          </div>
-          <div className={styles.buttonWrap}>
-            {ageButtons.map((buttonData, index) => (
-              <button
-                key={index}
-                onClick={() => setSelectedAge(buttonData.age)}
-                className={selectedAge === buttonData.age ? styles.selectedButton : ''}
-              >
-                {buttonData.label}
-              </button>
-            ))}
-          </div>
-          <div className={styles.totalKeywordBox}>
-            <ul>
-              {dataTotalList.map(data => (
-                <ol>
-                  {data.keywordRanks.slice(0, 10).map((rank, index) => (
-                    <li key={index}>
-                      <span className={styles.rank}>{index + 1}위</span>
-                      <span> {rank.keyword} +{rank.count} </span>
-                    </li>
-                  ))}
-                </ol>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.keywords}>
-            {getFilteredData().map(data => (
-              <div key={data.age}>
-                <ul>
-                  {data.keywordRanks.slice(0, 10).map((rank, index) => (
-                    <li key={index} className={styles.indexBox}>
-                      <span className={styles.rank}>{index + 1}위</span>
-                      <span> {rank.keyword} </span>
-                      <span style={{ color: "#00BFFF", fontWeight: "600"}}> +{rank.count} </span>
-                    </li>
-                  ))}
-                </ul>
+        <div className={styles.body}>
+          <div className={styles.wordCloudBox}>
+            {selectedAge !== null ? (
+              <div className={styles.wordCloud}>
+                <h3>{selectedAge === 0 ? "전체 워드 클라우드" : `${selectedAge}대 워드 클라우드`}</h3>
+                <WordCloud words={convertToWords(getFilteredData()[0]?.keywordRanks)} />
               </div>
-            ))}
+            ) : (
+              <div className={styles.wordCloud}>
+                <h3>전체 워드 클라우드</h3>
+                <WordCloud words={convertToWords(dataList.find(data => data.age === 0)?.keywordRanks || [])} />
+              </div>
+            )}
           </div>
-        </div>
+          <div className={styles.keywordBox}>
+            <div>
+              <p><h3>나이대별 키워드 순위</h3></p>
+            </div>
+            <div className={styles.buttonWrap}>
+              {ageButtons.map((buttonData, index) => (
+                <button
+                  key={index}
+                  onClick={() => setSelectedAge(buttonData.age)}
+                  className={selectedAge === buttonData.age ? styles.selectedButton : ''}
+                >
+                  {buttonData.label}
+                </button>
+              ))}
+            </div>
+            <div className={styles.totalKeywordBox}>
+              <ul>
+                {dataTotalList.map(data => (
+                  <ol>
+                    {data.keywordRanks.slice(0, 10).map((rank, index) => (
+                      <li key={index}>
+                        <span className={styles.rank}>{index + 1}위</span>
+                        <span> {rank.keyword} +{rank.count} </span>
+                      </li>
+                    ))}
+                  </ol>
+                ))}
+              </ul>
+            </div>
+            <div className={styles.keywords}>
+              {getFilteredData().map(data => (
+                <div key={data.age}>
+                  <ul>
+                    {data.keywordRanks.slice(0, 10).map((rank, index) => (
+                      <li key={index} className={styles.indexBox}>
+                        <span className={styles.rank}>{index + 1}위</span>
+                        <span> {rank.keyword} </span>
+                        <span style={{ color: "#00BFFF", fontWeight: "600"}}> +{rank.count} </span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>        
       </div>
     </>
   );

505553a89d057a9745a343bfb948a4a0bd502382
haesol822 <dordork@naver.com>
2023-09-03 13:01:37 +0900

    #38 feat: 메인페이지 전체 추가

--- src/pages/Workspace/Home.tsx
+++ src/pages/Workspace/Home.tsx
@@ -15,6 +15,7 @@ type ApiResponse = {
 
 export default function Home() {
   const [dataList, setDataList] = useState<ApiResponse[]>([]);
+  const [dataTotalList, ] = useState<ApiResponse[]>([]);
   const [selectedAge, setSelectedAge] = useState<number | null>(null);
 
   const fetchKeywordRanking = async () => {
@@ -30,17 +31,46 @@ export default function Home() {
     }
   };
 
+  const fetchKeywordTotal = async () => {
+    try {
+      const res = await axios.get<ApiResponse[]>(`https://www.assum.store/keywords/keywordTotalRanking`);
+      console.log('home.tsx 전체리스트 가져오기 성공', res);
+      // 0대 데이터를 dataList에 추가
+      const zeroAgeData = res.data.find(data => data.age === 0);
+      if (zeroAgeData) {
+        setDataList(prevDataList => [...prevDataList, zeroAgeData]);
+      }
+    } catch (err) {
+      console.error('home.tsx 전체리스트 서버 요청 실패:', err);
+    }
+  };
+
   // 선택된 연령대에 해당하는 데이터를 필터링하여 반환
   const getFilteredData = () => {
     if (selectedAge === null) {
-      return dataList; // 선택된 연령대가 없으면 전체 데이터 반환
+      const zeroAgeData = dataList.find(data => data.age === 0);
+      if (zeroAgeData) {
+        return [zeroAgeData];
+      }
+      return [];
     }
     return dataList.filter(data => data.age === selectedAge);
   };
 
   useEffect(() => {
     fetchKeywordRanking();
+    fetchKeywordTotal();
   }, []);
+
+  const ageButtons = [
+    { label: '전체', age: 0 },
+    { label: '10대', age: 10 },
+    { label: '20대', age: 20 },
+    { label: '30대', age: 30 },
+    { label: '40대', age: 40 },
+    { label: '50대', age: 50 },
+    { label: '60대 이상', age: 60 },
+  ];
 
   return (
     <>
@@ -49,17 +79,37 @@ export default function Home() {
         <div className={styles.header}>
           <div className={styles.title}>요약 키워드 순위</div>
         </div>
+        <div className={styles.wordCloudBox}>
+
+        </div>
         <div className={styles.keywordBox}>
           <div>
             <p><h3>나이대별 키워드 순위</h3></p>
           </div>
-          <div>
-            <button onClick={() => setSelectedAge(10)}>10대</button>
-            <button onClick={() => setSelectedAge(20)}>20대</button>
-            <button onClick={() => setSelectedAge(30)}>30대</button>
-            <button onClick={() => setSelectedAge(40)}>40대</button>
-            <button onClick={() => setSelectedAge(50)}>50대</button>
-            <button onClick={() => setSelectedAge(60)}>60대 이상</button>
+          <div className={styles.buttonWrap}>
+            {ageButtons.map((buttonData, index) => (
+              <button
+                key={index}
+                onClick={() => setSelectedAge(buttonData.age)}
+                className={selectedAge === buttonData.age ? styles.selectedButton : ''}
+              >
+                {buttonData.label}
+              </button>
+            ))}
+          </div>
+          <div className={styles.totalKeywordBox}>
+            <ul>
+              {dataTotalList.map(data => (
+                <ol>
+                  {data.keywordRanks.slice(0, 10).map((rank, index) => (
+                    <li key={index}>
+                      <span className={styles.rank}>{index + 1}위</span>
+                      <span> {rank.keyword} +{rank.count} </span>
+                    </li>
+                  ))}
+                </ol>
+              ))}
+            </ul>
           </div>
           <div className={styles.keywords}>
             {getFilteredData().map(data => (
@@ -69,7 +119,7 @@ export default function Home() {
                     <li key={index} className={styles.indexBox}>
                       <span className={styles.rank}>{index + 1}위</span>
                       <span> {rank.keyword} </span>
-                      <span> +{rank.count} </span>
+                      <span style={{ color: "#00BFFF", fontWeight: "600"}}> +{rank.count} </span>
                     </li>
                   ))}
                 </ul>

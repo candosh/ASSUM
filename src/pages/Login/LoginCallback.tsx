83842e49dd5a3659a147341dd68150d5764ddef5
choiseohyeon <csh200310@naver.com>
2024-01-03 02:15:20 +0900

    fix: 충돌해결

--- src/pages/Login/LoginCallback.tsx
+++ src/pages/Login/LoginCallback.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import lottie from "lottie-web";
